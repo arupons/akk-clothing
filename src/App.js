2eff665e6fbaca04345809106693ea2c4b1a4758
Alfonso Aguilar <aruponse@gmail.com>
2020-10-22 22:00:49 -0500

    Agrega Shoping Page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 
 const HatsPage = () => (
   <div>
@@ -13,6 +14,7 @@ function App() {
     <div>
       <switch>
         <Route exact path='/' component={HomePage} />
+        <Route exact path='/shop' component={ShopPage} />
         <Route exact path='/shop/hats' component={HatsPage} />
       </switch>
     </div>
